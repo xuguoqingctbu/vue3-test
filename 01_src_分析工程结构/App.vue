de95a59c810e846f69d038ca0abb204b80c9bfbd
xuguoqingctbu <1761368665@qq.com>
2022-01-13 17:06:47 +0800

    vue setup ref reactive

---
+++ 01_src_分析工程结构/App.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- Vue3组件中的模板结构可以没有根标签 -->
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 03_src_ref函数/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h3>工作种类：{{ job.type }}</h3>
+  <h3>工作薪水：{{ job.salary }}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  setup() {
+    //数据
+    let name = ref("李四");
+    let age = ref(18);
+    let job = ref({
+      type: "前端",
+      salary: "10K",
+    });
+    function changeInfo() {
+      name.value = "张三";
+      age.value = 20;
+      job.value.type = "JAVA开发工程师";
+      job.value.salary = "20K";
+    }
+    //返回一个对象（常用）
+    return {
+      name,
+      age,
+      job,
+      changeInfo,
+    };
+  },
+};
+</script>
+
+<style></style>
